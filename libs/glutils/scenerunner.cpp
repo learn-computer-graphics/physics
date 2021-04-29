e015b9da96f44304ba9fbc1a776d949280279263
Guillaume Haerinck <haerinck.guillaume@gmail.com>
2021-04-29 21:59:10 +0200

    [p01] Run imgui

---
+++ libs/glutils/scenerunner.cpp
@@ -0,0 +1,119 @@
+#include "scenerunner.h"
+
+#include "glutils.h"
+
+#include <glad/glad.h>
+#include <imgui/imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+SceneRunner::SceneRunner(const std::string& windowTitle, int width, int height, int samples) : debug(true) {
+	// Initialize GLFW
+	if (!glfwInit()) exit(EXIT_FAILURE);
+
+#ifdef __APPLE__
+	// Select OpenGL 4.1
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+#else
+	// Select OpenGL 4.6
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+#endif
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	if (debug)
+		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+	if (samples > 0) {
+		glfwWindowHint(GLFW_SAMPLES, samples);
+	}
+
+	// Open the window
+	window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, windowTitle.c_str(), NULL, NULL);
+	if (!window) {
+		std::cerr << "Unable to create OpenGL context." << std::endl;
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+
+	// Get framebuffer size
+	glfwGetFramebufferSize(window, &fbw, &fbh);
+
+	// Load the OpenGL functions.
+	if (!gladLoadGL()) { exit(-1); }
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 430 core");
+	ImGui::StyleColorsDark();
+
+	GLUtils::dumpGLInfo();
+
+	// Initialization
+	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+#ifndef __APPLE__
+	if (debug) {
+		glDebugMessageCallback(GLUtils::debugCallback, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
+		glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION, GL_DEBUG_TYPE_MARKER, 0,
+			GL_DEBUG_SEVERITY_NOTIFICATION, -1, "Start debugging");
+	}
+#endif
+}
+
+int SceneRunner::run(std::unique_ptr<Scene> scene) {
+	// Enter the main loop
+	mainLoop(window, std::move(scene));
+
+#ifndef __APPLE__
+	if (debug)
+		glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION, GL_DEBUG_TYPE_MARKER, 1,
+			GL_DEBUG_SEVERITY_NOTIFICATION, -1, "End debug");
+#endif
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	// Close window and terminate GLFW
+	glfwTerminate();
+
+	// Exit program
+	return EXIT_SUCCESS;
+}
+
+void SceneRunner::mainLoop(GLFWwindow* window, std::unique_ptr<Scene> scene) {
+	scene->setDimensions(fbw, fbh);
+	scene->initScene();
+	scene->resize(fbw, fbh);
+
+	while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE)) {
+		GLUtils::checkForOpenGLError(__FILE__, __LINE__);
+
+		scene->update(float(glfwGetTime()));
+		scene->render();
+		
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		{
+			ImGui::Begin("GUI");
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::End();
+		}
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwPollEvents();
+		int state = glfwGetKey(window, GLFW_KEY_SPACE);
+		if (state == GLFW_PRESS)
+			scene->animate(!scene->animating());
+
+		glfwSwapBuffers(window);
+	}
+}
--- libs/glutils/scenerunner.h
+++ libs/glutils/scenerunner.h
@@ -1,11 +1,11 @@
-
-#include "scene.h"
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include "glutils.h"
+#pragma once
 
 #define WIN_WIDTH 800
 #define WIN_HEIGHT 600
+
+#include "scene.h"
+
+#include <GLFW/glfw3.h>
 
 #include <map>
 #include <string>
@@ -14,98 +14,16 @@
 #include <memory>
 
 class SceneRunner {
+public:
+	SceneRunner(const std::string& windowTitle, int width = WIN_WIDTH, int height = WIN_HEIGHT, int samples = 0);
+	int run(std::unique_ptr<Scene> scene);
+
+private:
+	void mainLoop(GLFWwindow* window, std::unique_ptr<Scene> scene);
+
 private:
 	GLFWwindow* window;
-	int fbw, fbh;
-	bool debug;           // Set true to enable debug messages
-
-public:
-	SceneRunner(const std::string& windowTitle, int width = WIN_WIDTH, int height = WIN_HEIGHT, int samples = 0) : debug(true) {
-		// Initialize GLFW
-		if (!glfwInit()) exit(EXIT_FAILURE);
-
-#ifdef __APPLE__
-		// Select OpenGL 4.1
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-#else
-		// Select OpenGL 4.6
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-#endif
-		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-		glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-		if (debug)
-			glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
-		if (samples > 0) {
-			glfwWindowHint(GLFW_SAMPLES, samples);
-		}
-
-		// Open the window
-		window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, windowTitle.c_str(), NULL, NULL);
-		if (!window) {
-			std::cerr << "Unable to create OpenGL context." << std::endl;
-			glfwTerminate();
-			exit(EXIT_FAILURE);
-		}
-		glfwMakeContextCurrent(window);
-
-		// Get framebuffer size
-		glfwGetFramebufferSize(window, &fbw, &fbh);
-
-		// Load the OpenGL functions.
-		if (!gladLoadGL()) { exit(-1); }
-
-		GLUtils::dumpGLInfo();
-
-		// Initialization
-		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-#ifndef __APPLE__
-		if (debug) {
-			glDebugMessageCallback(GLUtils::debugCallback, nullptr);
-			glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
-			glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION, GL_DEBUG_TYPE_MARKER, 0,
-				GL_DEBUG_SEVERITY_NOTIFICATION, -1, "Start debugging");
-		}
-#endif
-	}
-
-	int run(std::unique_ptr<Scene> scene) {
-		// Enter the main loop
-		mainLoop(window, std::move(scene));
-
-#ifndef __APPLE__
-		if (debug)
-			glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION, GL_DEBUG_TYPE_MARKER, 1,
-				GL_DEBUG_SEVERITY_NOTIFICATION, -1, "End debug");
-#endif
-
-		// Close window and terminate GLFW
-		glfwTerminate();
-
-		// Exit program
-		return EXIT_SUCCESS;
-	}
-
-private:
-	void mainLoop(GLFWwindow* window, std::unique_ptr<Scene> scene) {
-
-		scene->setDimensions(fbw, fbh);
-		scene->initScene();
-		scene->resize(fbw, fbh);
-
-		while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE)) {
-			GLUtils::checkForOpenGLError(__FILE__, __LINE__);
-
-			scene->update(float(glfwGetTime()));
-			scene->render();
-			glfwSwapBuffers(window);
-
-			glfwPollEvents();
-			int state = glfwGetKey(window, GLFW_KEY_SPACE);
-			if (state == GLFW_PRESS)
-				scene->animate(!scene->animating());
-		}
-	}
+	int fbw;
+	int fbh;
+	bool debug;
 };
